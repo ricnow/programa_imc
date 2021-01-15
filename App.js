0189e87ffb9bc821d646363fea5d97a73c431565
Richelle Soares <richelle.soares@gmail.com>
2021-01-15 12:14:48 -0300

    Criando a parte de calculo do imc

--- App.js
+++ App.js
@@ -17,6 +17,34 @@ import {
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {altura: 0, massa: 0, resultado: 0, resultadoText: ''};
+    this.calcular = this.calcular.bind(this);
+  }
+  calcular() {
+    let imc = this.state.massa / (this.state.altura * this.state.altura);
+    let s = this.state;
+    s.resultado = imc;
+    if (s.resultado < 16) {
+      s.resultadoText = 'Magreza Grave';
+    } else if (s.resultado < 17) {
+      s.resultadoText = 'Magreza Moderada';
+    } else if (s.resultado < 18.5) {
+      s.resultadoText = 'Magreza Leve';
+    } else if (s.resultado < 25) {
+      s.resultadoText = 'Saudável';
+    } else if (s.resultado < 30) {
+      s.resultadoText = 'Sobrepeso';
+    } else if (s.resultado < 35) {
+      s.resultadoText = 'Obesidade Grau I';
+    } else if (s.resultado < 40) {
+      s.resultadoText = 'Obesidade Grau II';
+    } else {
+      s.resultadoText = 'Obesidade Grau III (Mórbido)';
+    }
+    this.setState(s);
+  }
   render() {
     return (
       <View style={styles.container}>
@@ -25,18 +53,26 @@ export default class App extends Component<Props> {
             placeholder="Massa"
             keyboardType="numeric"
             style={styles.input}
+            onChangeText={(massa) => {
+              this.setState({massa});
+            }}
           />
           <TextInput
             placeholder="Altura"
             keyboardType="numeric"
             style={styles.input}
+            onChangeText={(altura) => {
+              this.setState({altura});
+            }}
           />
         </View>
-        <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <TouchableOpacity style={styles.button} onPress={this.calcular}>
           <Text style={styles.texto}>Calcular</Text>
         </TouchableOpacity>
-        <Text style={styles.resultado}>30</Text>
-        <Text style={[styles.resultado, {fontSize: 30}]}>Obesidade 1</Text>
+        <Text style={styles.resultado}>{this.state.resultado.toFixed(2)}</Text>
+        <Text style={[styles.resultado, {fontSize: 30}]}>
+          {this.state.resultadoText}
+        </Text>
       </View>
     );
   }
